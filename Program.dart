4e6fd7b4b028a8e77fa85fa999041e8ba4abba41
Arnaud Goguelin <arnaud.goguelin@gmail.com>
2024-02-15 22:38:17 +0100

    implémentation de la logique du jeu

--- Program.dart
+++ Program.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:math';
 
 /* while true
   show prompt OK
@@ -15,6 +16,14 @@ else
 
 BONUS PERSO: établir un % de victoires / défaites
 */
+
+enum Move {
+  rock,
+  paper,
+  scissors,
+  waiting,
+}
+
 void main() {
   while (true) {
     stdout.write("""
@@ -22,21 +31,45 @@ Rock, paper or scissors?
 Press: 'r' or 'p' or 's'to play.
 Press 'q' to exit the game.
 Your input: """);
+    final randomNumber = Random().nextInt(3);
+    final AIChoice = Move.values[randomNumber];
+
     final input = stdin.readLineSync();
+    var playerChoice = Move.values[3];
+
     switch (input) {
       case 'q':
         exit(0);
       case 'r':
+        playerChoice = Move.rock;
         break;
       case 'p':
+        playerChoice = Move.values[1];
         break;
       case 's':
+        playerChoice = Move.scissors;
         break;
       default:
         print("""
 $input is an invalid input. Please choose a valid one.
         """);
-        break;
+        continue;
+    }
+
+    print('AI played ${AIChoice.name}');
+    print('You played ${playerChoice.name}');
+
+    if (playerChoice == AIChoice) {
+      print("""It\'s a draw.
+            """);
+    } else if (playerChoice == Move.rock && AIChoice == Move.scissors ||
+        playerChoice == Move.scissors && AIChoice == Move.paper ||
+        playerChoice == Move.paper && AIChoice == Move.rock) {
+      print("""You win!
+            """);
+    } else {
+      print("""You loose.
+            """);
     }
   }
 }
